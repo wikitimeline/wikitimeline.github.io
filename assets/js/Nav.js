4e44748131b354094fe9cc4e48c3861c4dfa56c3
Matthew Grouse <72737644+MatthewGrouse@users.noreply.github.com>
2021-03-05 21:17:21 +0000

    added navigation bar

---
+++ assets/js/Nav.js
@@ -0,0 +1,9 @@
+
+    function openTab(tabName) {
+    var i;
+    var x = document.getElementsByClassName("panel");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    document.getElementById(tabName).style.display = "block";  
+  }
